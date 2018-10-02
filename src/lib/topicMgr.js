894a3ea3d252836050d8743e13a5637266cd8faf
Rob Whitaker <rmw2@princeton.edu>
2018-10-02 11:28:52 -0400

    doc: extract SQL queries to string constants, add docstring for topicMgr

--- src/lib/topicMgr.js
+++ src/lib/topicMgr.js
@@ -1,6 +1,31 @@
 import { Client } from 'pg'
 
+const CREATE_QUERY = `
+  INSERT INTO topics(id, expiration) 
+  VALUES ($1, now() + interval '$2' second);
+`
+const READ_QUERY = `
+  SELECT * FROM topics
+  WHERE id=$1
+  AND expiration > now();
+`
+const UPDATE_QUERY = `
+  UPDATE topics 
+  SET content = $2 
+  WHERE id = $1;
+`
+const DELETE_QUERY = `
+  DELETE FROM topics
+  WHERE id = $1;
+`
 
+/**
+ * @classdesc
+ * Main class for managing topics on the Chasqui service.  A topic is simply
+ * an id with a message associated with it that supports CRUD operations,
+ * each of which is implemented as an asynchronous method of this class.
+ * 
+ */
 class TopicMgr {
 
     constructor() {
@@ -26,10 +51,7 @@ class TopicMgr {
 
         try {
             await client.connect()
-            const res = await client.query(
-                "INSERT INTO topics(id, expiration) \
-             VALUES ($1, now() + interval '$2' second);"
-                , [topicId, this.expiration]);
+            const res = await client.query(CREATE_QUERY, [topicId, this.expiration]);
             return res.rows[0];
         } catch (e) {
             throw (e);
@@ -48,10 +70,7 @@ class TopicMgr {
 
         try {
             await client.connect()
-            const res = await client.query(
-                "SELECT * FROM topics \
-                    WHERE id=$1 \
-                    AND expiration > now()", [topicId]);
+            const res = await client.query(READ_QUERY, [topicId]);
             return res.rows[0];
         } catch (e) {
             throw (e);
@@ -71,11 +90,7 @@ class TopicMgr {
 
         try {
             await client.connect()
-            const res = await client.query(
-                "UPDATE topics SET \
-                content = $2 WHERE \
-                id = $1;"
-                , [topicId, content]);
+            const res = await client.query(UPDATE_QUERY, [topicId, content]);
             return;
         } catch (e) {
             throw (e);
@@ -94,20 +109,14 @@ class TopicMgr {
 
         try {
             await client.connect()
-            const res = await client.query(
-                "DELETE FROM topics \
-                WHERE id = $1;"
-                , [topicId]);
+            const res = await client.query(DELETE_QUERY, [topicId])
             return;
-
         } catch (e) {
             throw (e);
         } finally {
             await client.end()
         }
     }
-
-
 }
 
 module.exports = TopicMgr
