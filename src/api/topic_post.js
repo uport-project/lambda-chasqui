8200473bd734dfa0986e2591dcf9f69bad8a4109
Rob Whitaker <rmw2@princeton.edu>
2018-10-02 11:28:12 -0400

    feat: allow topic creation from post request, same as get

--- src/api/topic_post.js
+++ src/api/topic_post.js
@@ -4,38 +4,51 @@ class TopicPostHandler {
         this.topicMgr = topicMgr
     }
 
+    /**
+     * Handle a post request for a given topic ID, by creating one if none
+     * exists, or updating an existing topic.  Respond by calling cb with 
+     * message 'created' or 'updated' as appropriate
+     * @param {Object} event 
+     * @param {Object} context 
+     * @param {Function} cb 
+     */
     async handle(event, context, cb) {
-
-        //Parse body
-        let body;
+        // Parse body
+        let body
         try {
             body = JSON.parse(event.body)
         } catch (e) {
             cb({ code: 403, message: 'no json body'})
-            return;
+            return
         }
 
         if (event.pathParameters && event.pathParameters.id) {
-            let topicId;
+            let topicId, message = 'updated'
             topicId = event.pathParameters.id
             try {
+                // First check for existing topic
+                topic = await this.topicMgr.read(topicId)
+                if (!topic) {
+                  // Create topic if none exists, and set response message to 'created'
+                  message = 'created'
+                  topic = await this.topicMgr.create(topicId)
+                  console.log("topic created: " + topicId)
+                }
                 await this.topicMgr.update(topicId, body)
                 console.log("topic updated: " + topicId)
             } catch (error) {
                 console.log("Error on this.topicMgr.update")
                 console.log(error)
                 cb({ code: 500, message: error.message })
-                return;
+                return
             }
-            cb(null, { message: "updated" });
-            return;
+            cb(null, { message })
+            return
         } else {
-            cb({ code: 400, message: "No topic id" });
-            return;
+            cb({ code: 400, message: "No topic id" })
+            return
         }
-
     }
-
 }
 
 module.exports = TopicPostHandler
