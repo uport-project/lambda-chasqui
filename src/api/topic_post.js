1bb223a6c73993e5b19e33b6aa7142306f2cfd94
Rob Whitaker <rmw2@princeton.edu>
2018-10-03 12:08:46 -0400

    Revert "feat: allow topic creation from post request, same as get"
    This reverts commit 8200473bd734dfa0986e2591dcf9f69bad8a4109.

--- src/api/topic_post.js
+++ src/api/topic_post.js
@@ -4,36 +4,21 @@ class TopicPostHandler {
         this.topicMgr = topicMgr
     }
 
-    /**
-     * Handle a post request for a given topic ID, by creating one if none
-     * exists, or updating an existing topic.  Respond by calling cb with 
-     * message 'created' or 'updated' as appropriate
-     * @param {Object} event 
-     * @param {Object} context 
-     * @param {Function} cb 
-     */
     async handle(event, context, cb) {
-        // Parse body
-        let body
+
+        //Parse body
+        let body;
         try {
             body = JSON.parse(event.body)
         } catch (e) {
             cb({ code: 403, message: 'no json body'})
-            return
+            return;
         }
 
         if (event.pathParameters && event.pathParameters.id) {
-            let topicId, message = 'updated'
+            let topicId
             topicId = event.pathParameters.id
             try {
-                // First check for existing topic
-                let topic = await this.topicMgr.read(topicId)
-                if (!topic) {
-                  // Create topic if none exists, and set response message to 'created'
-                  message = 'created'
-                  topic = await this.topicMgr.create(topicId)
-                  console.log("topic created: " + topicId)
-                }
                 await this.topicMgr.update(topicId, body)
                 console.log("topic updated: " + topicId)
             } catch (error) {
@@ -42,13 +27,27 @@ class TopicPostHandler {
                 cb({ code: 500, message: error.message })
                 return
             }
-            cb(null, { message })
-            return
+            cb(null, { message: "updated" });
+            return;
         } else {
-            cb({ code: 400, message: "No topic id" })
-            return
+          // Create a new topic
+          let topicId = randomString(16)
+          let topic = await this.topicMgr.read(topicId)
+          while (topic) {
+            topicId = randomString(16)
+            topic = await this.topicMgr.read(topicId)
+          }
+
+          // Create topic if none exists, and set response message to 'created'
+          message = 'created'
+          topic = await this.topicMgr.create(topicId)
+          console.log("topic created: " + topicId)
+          cb(null, { message: "created" })
+          return
         }
+
     }
+
 }
 
 module.exports = TopicPostHandler
